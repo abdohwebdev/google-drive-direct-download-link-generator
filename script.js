e4396c18730dcf8e9bd2d7b99e919959d2f8a433
Abdurrahman Abulgasim <super12226@gmail.com>
2021-10-03 15:06:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ Google Drive Direct Download Link Generator
 Created by Abdurrahman Abulgasim
 */
 /***************** get preview link *************************/
-let link = 'https://drive.google.com/file/d/1twcRQIUL-mfhn7pCmOJdhX-D5M0oCzE0/view';//sample preview link from google drive
+let link = //here goes your preview link
 /***************** get file id from the link ****************/
 const re = /d\/(.*)\/view/g;
 const array = [...link.matchAll(re)];
